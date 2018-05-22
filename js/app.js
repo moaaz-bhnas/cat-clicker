e7b93ca248d00baa011e45d37ee9e338f7d862b7
moaaz <moaaz_bs@yahoo.com>
2018-05-22 05:19:31 +0200

    Kteer Fashkh :'D remove, add ..

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 'use strict';
 
+/* --- Page Current Viewed Cat --- */
+let currentCat;
+
 /* --- Dom Elements --- */
 const catsNamesList = document.querySelector('.cats-list'),
-      catTemplate = {
+      catView = {
           container: document.querySelector('.cat'),
           clicksCount: document.querySelector('.clicks-record'),
           name: document.querySelector('.name'),
-          image: document.querySelector('.cat-image')
+          image: document.querySelector('.cat-image'),
+          
+          empty() {
+              this.clicksCount.classList.remove('show');
+              this.clicksCount.textContent = '';
+              this.name.textContent = '';
+              this.image.setAttribute('src', '');
+          }
+      },
+      addCatForm = document.querySelector('.add-cat'),
+      catAddSign = document.querySelector('.add'),
+      newCat = {
+          name: document.querySelector('.new-cat-name'),
+          src: document.querySelector('.new-cat-src'),
+          color: document.querySelector('.new-cat-color'),
+          addBtn: document.querySelector('button'),
+          
+          emptyInputs() {
+              this.name.value = '';
+              this.src.value = '';
+              this.color.value = '';
+          },
+          
+          resetColor() {
+              this.color.style.color = 'inherit';
+          }
       };
 
 /* --- Cat Class --- */
@@ -16,7 +44,10 @@ class Cat {
         this.clicksCount = 0;
         this.name = name;
         this.src = src;
-        this.listItem = `<li class="${this.name.toLowerCase()}" style="color: ${this.color}">${this.name}</li>`;
+        this.listItem = `<li class="${(this.name).toLowerCase()}" style="color: ${this.color}">
+                            <span class="cat-name">${this.name}</span>
+                            <span class="remove fa fa-times" title="remove" aria-hidden="true"></span>
+                        </li>`;
     }
     
     listCatName() {
@@ -24,11 +55,15 @@ class Cat {
     }
     
     setColor() {
-        catTemplate.container.style.color = this.color;
+        catView.container.style.color = this.color;
+        document.body.style.backgroundColor = this.color;
+        addCatForm.style.color = this.color;
+        newCat.addBtn.style.backgroundColor = this.color;
+        catAddSign.style.color = this.color;
     }
     
     logClicksRecord() {
-        catTemplate.clicksCount.textContent = this.clicksCount;
+        catView.clicksCount.textContent = this.clicksCount;
     }
     
     incrementClicksRecord() {
@@ -36,37 +71,28 @@ class Cat {
     }
     
     logName() {
-        catTemplate.name.textContent = this.name;
+        catView.name.textContent = this.name;
     }
     
     setImage() {
-        catTemplate.image.setAttribute('src', this.src);
+        catView.image.setAttribute('src', this.src);
     }
     
     render() {
         this.setColor();
         this.logClicksRecord();
-        catTemplate.clicksCount.classList.add('show');
+        catView.clicksCount.classList.add('show');
         this.logName();
         this.setImage();
     }
 }
 
 /* --- Instances --- */
-const cats = new Map([['tom', new Cat('#C8CECE', 'Tom', 'images/tom.png')],
+const cats = new Map([['tom', new Cat('#90979F', 'Tom', 'images/tom.png')],
                       ['garfield', new Cat('#FCAA16', 'Garfield', 'images/garfield.png')],
                       ['marie', new Cat('#F06594', 'Marie', 'images/marie.png')],
                       ['meowth', new Cat('#DF7645', 'Meowth', 'images/meowth.png')],
                       ['sylvester', new Cat('#1F2831', 'Sylvester', 'images/sylvester.png')]]);
-
-/* --- Cat List --- */
-const renderNamesList = () => {
-    for (const cat of cats) {
-        const [key, obj] = cat;
-        obj.listCatName();
-    }
-}
-renderNamesList();
 
 /* --- Cache Images --- */
 const images = [];
@@ -77,29 +103,140 @@ const preloadImage = src => {
     images.push(image);
 }
 
-for (const cat of cats) {
-    const [key, object] = cat;
-    preloadImage(object.src);
+const preloadImages = () => {
+    for (const cat of cats) {
+        const [key, object] = cat;
+        preloadImage(object.src);
+    }
+}
+preloadImages();
+
+/* --- Cat List --- */
+const renderNamesList = () => {
+    for (const cat of cats) {
+        const [key, obj] = cat;
+        obj.listCatName();
+    }
+}
+renderNamesList();
+
+/* --- Display Random Cat --- */
+const fadeOutIn = catObj => {
+    catView.container.classList.add('fade-out');
+    setTimeout(() => {
+        catObj.render();
+        catView.container.classList.remove('fade-out');
+    }, 150);
+}
+
+const displayRandomCat = () => {
+    const catNames = [];
+    for (const cat of cats) {
+        const [name, object] = cat;
+        catNames.push(name);
+    }
+    const randomIndex = Math.floor(Math.random() * catNames.length);
+    currentCat = catNames[randomIndex];
+    const randomCat = cats.get(catNames[randomIndex]);
+    
+    fadeOutIn(randomCat);
+}
+
+displayRandomCat();
+
+/* --- Random Color Generator --- */
+const generateRandomColor = () => {
+    const characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        let randomIndex = Math.floor(Math.random() * 16);
+        color += characters[randomIndex];
+    }
+    return color;
 }
 
 /* ----- Click Events ----- */
-/* --- Select Cat --- */
+/* --- Select and Remove Cats --- */
 catsNamesList.addEventListener('click', event => {
-    if (event.target.nodeName === 'LI') {
-        const catName = event.target.textContent.toLowerCase();
-        const currentCat = cats.get(catName);
-        currentCat.render();
+    
+    const nameOrXClicked = event.target.nodeName === 'SPAN';
+    
+    if (nameOrXClicked) {
+        const catLi = event.target.parentElement,
+              clickedCatName = catLi.className; // Chloe
+        
+        switch (event.target.className) {
+            // Select
+            case 'cat-name':
+                if (currentCat !== clickedCatName) {
+                    currentCat = clickedCatName;
+                    const catObject = cats.get(clickedCatName); // {color: "#C8CECE", clicksCount: 0, name: "Tom", src: "images/tom.png", listItem: "<li ..
+                    fadeOutIn(catObject);
+                }
+                break;
+            // Remove
+            case 'remove fa fa-times':
+                catLi.remove();
+                cats.delete(clickedCatName);
+                if (cats.size === 0) {
+                    catView.empty()
+                    document.body.style.backgroundColor = 'transparent';
+                    catsNamesList.style.padding = 0;
+                    addCatForm.style.color = 'grey';
+                    newCat.addBtn.style.backgroundColor = 'grey';
+                    catAddSign.style.color = 'grey';
+                } else {
+                    displayRandomCat();
+                }   
+        }
+        
     }
 });
 
 /* --- Click Cat --- */
-catTemplate.image.addEventListener('click', event => {
-    const catName = event.target.parentElement.querySelector('.name').textContent.toLowerCase();
-    const currentCat = cats.get(catName); 
+catView.image.addEventListener('click', event => {
+    const clickedCatName = event.target.parentElement.querySelector('.name').textContent.toLowerCase();
+    const currentCat = cats.get(clickedCatName); 
     
     currentCat.incrementClicksRecord();
     currentCat.logClicksRecord();
 });
 
+/* --- Add Cat --- */
+addCatForm.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    catsNamesList.style.padding = '.5em';
+    
+    let name = newCat.name.value.toLowerCase(),
+        src = newCat.src.value,
+        color;
+    
+    if (newCat.color.style.color === 'inherit') {
+        color = generateRandomColor();   
+    } else {
+        color = newCat.color.value;
+    }
+    
+    cats.set(name, new Cat(color, (name[0].toUpperCase() + name.slice(1).toLowerCase()), src));
+    
+    catsNamesList.innerHTML = '';
+    
+    renderNamesList();
+    preloadImages();
+    newCat.emptyInputs();
+    
+    newCat.resetColor();
+});
 
+/* --- Color Input --- */
+newCat.color.addEventListener('input', function() {
+    this.style.color = 'inherit';
+    this.style.color = newCat.color.value;
+});
 
+/* --- pop-up add sign --- */
+catAddSign.addEventListener('click', function() {
+    this.classList.toggle('move');
+    addCatForm.classList.toggle('pop-up');
+});
